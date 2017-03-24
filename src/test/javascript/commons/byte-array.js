9b81fe96dfcc8cabe13ef79f8b0aa2b35c90ab2c
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-03-24 13:26:39 +0100

    Import-Statements vereinheitlicht.

--- src/test/javascript/commons/byte-array.js
+++ src/test/javascript/commons/byte-array.js
@@ -1,5 +1,5 @@
-import {describe, it} from "mocha";
-import {expect} from "chai";
+import { describe, it } from "mocha";
+import { expect } from "chai";
 
 import ByteArray from "../../../main/javascript/commons/byte-array";
 
