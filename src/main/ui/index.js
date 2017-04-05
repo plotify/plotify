98499ea78d4bcf8f9543da44cd61e862367a5aab
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-04-05 17:00:55 +0200

    Beispiel für das Erstellen einer neuen Geschichte über IPC hinzugefügt.

--- src/main/ui/index.js
+++ src/main/ui/index.js
@@ -14,6 +14,12 @@ import { Provider } from "react-redux";
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
+/* Beispiel für das Erstellen einer neuen Geschichte:
+import { sendToModel } from "../shared/commons/ipc";
+import { CREATE_STORY } from "../shared/stories/ipc-channels";
+sendToModel(CREATE_STORY).then(path => console.log("New story: " + path));
+*/
+
 const store = createStore((state = {}, action) => { return state; });
 
 class App extends React.Component {
