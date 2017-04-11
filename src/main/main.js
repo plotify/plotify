1b5c3b9d4aaba6709f566a19a9f0af39580547c8
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-04-11 15:08:12 +0200

    #13 Die Größe und Position des Hauptfensters wird beim Schließen des Fensters gespeichert und beim erneuten Öffnen wiederhergestellt.

--- src/main/main.js
+++ src/main/main.js
@@ -1,6 +1,8 @@
 import electron from "electron";
+import path from "path";
 import url from "url";
 import isDev from "electron-is-dev";
+import { getMainWindowPreferences, saveMainWindowPreferences } from "./model/user/main-window";
 import { setMainWindow } from "./model/main-window";
 
 import "./model/index";
@@ -9,45 +11,67 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
+let mainWindowBounds;
+let mainWindowMaximized;
 
 function createWindow() {
+  getMainWindowPreferences().then(preferences => {
 
-  let path = require("path");
+    mainWindow = new BrowserWindow({
+      width: preferences.width,
+      height: preferences.height,
+      x: preferences.x,
+      y: preferences.y,
+      icon: path.join(__dirname, "./ui/resources/app-icons/64.png"),
+      show: false
+    });
 
-  mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 600,
-    icon: path.join(__dirname, "./ui/resources/app-icons/64.png"),
-    show: false,
+    setMainWindow(mainWindow);
+
+    mainWindowBounds = mainWindow.getBounds();
+    mainWindowMaximized = mainWindow.isMaximized();
+
+    mainWindow.on("resize", updateCachedMainWindowPreferences);
+    mainWindow.on("move", updateCachedMainWindowPreferences);
+    mainWindow.on("maximize", updateCachedMainWindowPreferences);
+    mainWindow.on("unmaximize", updateCachedMainWindowPreferences);
+
+    mainWindow.on("ready-to-show", () => {
+
+      if (preferences.maximize) {
+        mainWindow.maximize();
+      }
+
+      if (isDev) {
+        mainWindow.openDevTools();
+      }
+
+      mainWindow.show();
+      mainWindow.focus();
+
+    });
+
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, "./ui/index.html"),
+      protocol: "file:",
+      slashes: true
+    }));
+
+    mainWindow.on("closed", () => {
+      saveMainWindowPreferences(mainWindowBounds, mainWindowMaximized);
+      mainWindow = null;
+      setMainWindow(null);
+    });
+
   });
-
-  setMainWindow(mainWindow);
-
-  mainWindow.on("ready-to-show", () => {
-
-    mainWindow.show();
-    mainWindow.focus();
-    mainWindow.maximize();
-
-    if (isDev) {
-      mainWindow.openDevTools();
-    }
-
-  });
-
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, "./ui/index.html"),
-    protocol: "file:",
-    slashes: true
-  }));
-
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
-
 }
 
-app.on("ready", createWindow);
+function updateCachedMainWindowPreferences() {
+  mainWindowBounds = mainWindow.getBounds();
+  mainWindowMaximized = mainWindow.isMaximized();
+}
+
+app.on("ready", () => createWindow());
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
---
+++ src/main/model/user/main-window.js
@@ -0,0 +1,81 @@
+import path from "path";
+import fs from "mz/fs";
+import app from "../../shared/commons/app";
+
+const directory = app.getPath("userData");
+const file = path.join(directory, "main-window.json");
+const charset = "utf-8";
+
+export function getMainWindowPreferences() {
+  return new Promise((resolve, reject) => {
+    loadMainWindowPreferences()
+      .then(preferences => resolve(preferences))
+      .catch(error => resolve(getDefaultMainWindowPreferences()));
+  });
+}
+
+function loadMainWindowPreferences() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, charset)
+      .then(data => parseMainWindowPreferences(data))
+      .then(json => resolve(json))
+      .catch(error => reject(error));
+  });
+}
+
+function parseMainWindowPreferences(data) {
+  return new Promise((resolve, reject) => {
+
+    const json = JSON.parse(data);
+
+    if (hasValidWidth(json) && hasValidHeight(json) &&
+        hasValidPositionX(json) && hasValidPositionY(json)) {
+      resolve(json);
+    } else {
+      reject();
+    }
+
+  });
+}
+
+function hasValidWidth(json) {
+  return json.hasOwnProperty("width") && Number.isInteger(json.width) && json.width > 0;
+}
+
+function hasValidHeight(json) {
+  return json.hasOwnProperty("height") && Number.isInteger(json.height) && json.height > 0;
+}
+
+function hasValidPositionX(json) {
+  return json.hasOwnProperty("x") && Number.isInteger(json.x);
+}
+
+function hasValidPositionY(json) {
+  return json.hasOwnProperty("y") && Number.isInteger(json.y);
+}
+
+function hasValidMaximize(json) {
+  return json.hasOwnProperty("maximize") && typeof json.maximize === "boolean";
+}
+
+function getDefaultMainWindowPreferences() {
+  return {
+    width: 1000,
+    height: 600,
+    x: undefined,
+    y: undefined,
+    maximized: true
+  };
+}
+
+export function saveMainWindowPreferences(bounds, maximized) {
+  const preferences = {
+    width: bounds.width,
+    height: bounds.height,
+    x: bounds.x,
+    y: bounds.y,
+    maximized: maximized
+  };
+  fs.truncateSync(file, 0);
+  fs.writeFileSync(file, JSON.stringify(preferences));
+}
