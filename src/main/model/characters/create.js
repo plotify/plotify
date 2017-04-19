76034064aefe12c22dd0b91d64fb6eff7f0a3c7b
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-04-19 18:43:02 +0200

    #85 Erstellen eines Charakters aufgeräumt. Definition des Standard-Steckbriefs in ein eigenes Modul ausgelagert.

--- src/main/model/characters/create.js
+++ src/main/model/characters/create.js
@@ -1,212 +1,9 @@
-import UUID from "../../shared/commons/uuid";
 import { sendCallback } from "../../shared/commons/ipc";
 import { CREATE_CHARACTER } from "../../shared/characters/ipc-channels";
-import { getConnection } from "../stories/connection";
 import { run, beginTransaction, endTransaction, rollbackTransaction } from "../shared/sqlite";
 
-export function createCharacter() {
-  return new Promise((resolve, reject) => {
-
-    const db = getConnection();
-
-    const characterId = UUID.random().toString();
-    const historyId = UUID.random().toString();
-
-    const insertHistorySql = "INSERT INTO character_history (id, name, deleted) VALUES (?, ?, ?)";
-    const insertHistoryParams = [historyId, "", 0];
-
-    const insertCharacterSql = "INSERT INTO character (id, presence_history_id) VALUES (?, ?)";
-    const insertCharacterParams = [characterId, historyId];
-
-    beginTransaction()
-      .then(() => run(insertHistorySql, insertHistoryParams))
-      .then(() => run(insertCharacterSql, insertCharacterParams))
-      .then(() => createDefaultProfile(characterId))
-      .then(() => endTransaction())
-      .then(() => resolve(characterId))
-      .catch(error => {
-        rollbackTransaction()
-          .then(() => reject(error))
-          .catch(() => reject(error));
-      });
-
-  });
-}
-
-const defaultProfile = [
-  {
-    title: "Name",
-    entries: ["Namensbedeutung", "Weitere Namen"]
-  },
-  {
-    title: "Physis",
-    entries: ["Alter", "Größe", "Geschlecht", "Gewicht", "Gestalt & Körperbau", "Gesundheit",
-              "Besondere körperliche Merkmale"]
-  },
-  {
-    title: "Aussehen",
-    entries: ["Gesicht", "Augen", "Haare", "Bart", "Kleidung", "Schmuck",
-              "Besondere äußerliche Merkmale"]
-  },
-  {
-    title: "Auftreten",
-    entries: ["Ausstrahlung", "Körperhaltung", "Stimme", "Immer dabei"]
-  },
-  {
-    title: "Gesellschaft",
-    entries: ["Herkunft", "Sprache", "Wohnort", "Beruf", "Stand"]
-  },
-  {
-    title: "Innere Eigenschaften",
-    entries: ["Charakter", "Interessen", "Abneigungen", "Moral", "Träume & Wünsche", "Ziele",
-              "Innerer Konflikt"]
-  },
-  {
-    title: "Veränderungen",
-    entries: ["Kindheit", "Jugend", "Erwachsenenalter", "Hohes Alter"]
-  },
-  {
-    title: "Sonstiges",
-    entries: ["Fähigkeiten", "Anmerkungen"]
-  }
-];
-
-export function createDefaultProfile(characterId) {
-  return new Promise((resolve, reject) => {
-
-    let promises = [];
-
-    for (let position = 0; position < defaultProfile.length; position++) {
-      const group = defaultProfile[position];
-      promises.push(handleGroup(characterId, group.title, group.entries, position));
-    }
-
-    Promise.all(promises)
-      .then(() => resolve())
-      .catch(error => reject(error));
-
-  });
-}
-
-function handleGroup(characterId, title, entries, position) {
-  return new Promise((resolve, reject) => {
-    insertGroupHistory(title, entries, position)
-      .then(historyId => insertGroup(characterId, historyId))
-      .then(groupId => handleEntries(characterId, groupId, entries))
-      .then(() => resolve())
-      .catch(error => reject(error));
-  });
-}
-
-function insertGroupHistory(title, entries, position) {
-  return new Promise((resolve, reject) => {
-
-    const historyId = UUID.random().toString();
-
-    const sql = " INSERT INTO entry_group_history " +
-                " (id, title, position, deleted)  " +
-                " VALUES (?, ?, ?, ?)             ";
-    const params = [historyId, title, position, 0];
-
-    getConnection().run(sql, params, (error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(historyId);
-      }
-    });
-
-  });
-}
-
-function insertGroup(characterId, historyId) {
-  return new Promise((resolve, reject) => {
-
-    const id = UUID.random().toString();
-
-    const sql = " INSERT INTO entry_group                 " +
-                " (character_id, id, presence_history_id) " +
-                " VALUES (?, ?, ?)                        ";
-    const params = [characterId, id, historyId];
-
-    getConnection().run(sql, params, (error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(id);
-      }
-    });
-
-  });
-}
-
-function handleEntries(characterId, groupId, entries) {
-  return new Promise((resolve, reject) => {
-
-    let promises = [];
-
-    for (let position = 0; position < entries.length; position++) {
-      const title = entries[position];
-      promises.push(handleEntry(characterId, groupId, title, position));
-    }
-
-    Promise.all(promises)
-      .then(() => resolve())
-      .catch(error => reject(error));
-
-  });
-}
-
-function handleEntry(characterId, groupId, title, position) {
-  return new Promise((resolve, reject) => {
-    insertEntryHistory(groupId, title, position)
-      .then(historyId => insertEntry(characterId, historyId))
-      .then(() => resolve())
-      .catch(error => reject(error));
-  });
-}
-
-function insertEntryHistory(groupId, title, position) {
-  return new Promise((resolve, reject) => {
-
-    const historyId = UUID.random().toString();
-
-    const sql = " INSERT INTO entry_history                       " +
-                " (id, group_id, title, value, position, deleted) " +
-                " VALUES (?, ?, ?, ?, ?, ?)                       ";
-    const params = [historyId, groupId, title, "", position, 0];
-
-    getConnection().run(sql, params, (error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(historyId);
-      }
-    });
-
-  });
-}
-
-function insertEntry(characterId, historyId) {
-  return new Promise((resolve, reject) => {
-
-    const id = UUID.random().toString();
-
-    const sql = " INSERT INTO entry                       " +
-                " (character_id, id, presence_history_id) " +
-                " VALUES (?, ?, ?)                        ";
-    const params = [characterId, id, historyId];
-
-    getConnection().run(sql, params, (error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
-      }
-    });
-
-  });
-}
+import UUID from "../../shared/commons/uuid";
+import defaultProfile from "./default-profile";
 
 export function registerCreateCharacterIpcChannel(ipcMain) {
   ipcMain.on(CREATE_CHARACTER, (event, payload) => {
@@ -214,4 +11,108 @@ export function registerCreateCharacterIpcChannel(ipcMain) {
       .then(characterId => sendCallback(event, payload, characterId))
       .catch(error => sendCallback(event, payload, error, false));
   });
+}
+
+export function createCharacter() {
+  const characterId = UUID.random().toString();
+  return Promise.resolve()
+    .then(() => beginTransaction())
+    .then(() => createEntry(characterId))
+    .then(() => createDefaultProfile(characterId))
+    .then(() => endTransaction(characterId))
+    .catch(error => {
+      console.log("Failed to create a new character: ", error);
+      return rollbackTransaction(error);
+    });
+}
+
+function createEntry(characterId) {
+
+  const historyId = UUID.random().toString();
+
+  const insertHistorySql = "INSERT INTO character_history (id, name, deleted) VALUES (?, ?, ?)";
+  const insertHistoryParams = [historyId, "", 0];
+
+  const insertCharacterSql = "INSERT INTO character (id, presence_history_id) VALUES (?, ?)";
+  const insertCharacterParams = [characterId, historyId];
+
+  return Promise.resolve()
+    .then(() => run(insertHistorySql, insertHistoryParams))
+    .then(() => run(insertCharacterSql, insertCharacterParams));
+
+}
+
+function createDefaultProfile(characterId) {
+
+  let promises = [];
+
+  for (let position = 0; position < defaultProfile.length; position++) {
+    const group = defaultProfile[position];
+    promises.push(handleGroup(characterId, group.title, group.entries, position));
+  }
+
+  return Promise.all(promises).then(() => Promise.resolve(characterId));
+
+}
+
+function handleGroup(characterId, title, entries, position) {
+  return Promise.resolve()
+    .then(() => insertGroupHistory(title, entries, position))
+    .then(historyId => insertGroup(characterId, historyId))
+    .then(groupId => handleEntries(characterId, groupId, entries));
+}
+
+function insertGroupHistory(title, entries, position) {
+  const historyId = UUID.random().toString();
+  const sql = " INSERT INTO entry_group_history " +
+              " (id, title, position, deleted)  " +
+              " VALUES (?, ?, ?, ?)             ";
+  const params = [historyId, title, position, 0];
+  return run(sql, params).then(() => Promise.resolve(historyId));
+}
+
+function insertGroup(characterId, historyId) {
+  const id = UUID.random().toString();
+  const sql = " INSERT INTO entry_group                 " +
+              " (character_id, id, presence_history_id) " +
+              " VALUES (?, ?, ?)                        ";
+  const params = [characterId, id, historyId];
+  return run(sql, params).then(() => Promise.resolve(id));
+}
+
+function handleEntries(characterId, groupId, entries) {
+
+  let promises = [];
+
+  for (let position = 0; position < entries.length; position++) {
+    const title = entries[position];
+    promises.push(handleEntry(characterId, groupId, title, position));
+  }
+
+  return Promise.all(promises);
+
+}
+
+function handleEntry(characterId, groupId, title, position) {
+  return Promise.resolve()
+    .then(() => insertEntryHistory(groupId, title, position))
+    .then(historyId => insertEntry(characterId, historyId));
+}
+
+function insertEntryHistory(groupId, title, position) {
+  const historyId = UUID.random().toString();
+  const sql = " INSERT INTO entry_history                       " +
+              " (id, group_id, title, value, position, deleted) " +
+              " VALUES (?, ?, ?, ?, ?, ?)                       ";
+  const params = [historyId, groupId, title, "", position, 0];
+  return run(sql, params).then(() => Promise.resolve(historyId));
+}
+
+function insertEntry(characterId, historyId) {
+  const id = UUID.random().toString();
+  const sql = " INSERT INTO entry                       " +
+              " (character_id, id, presence_history_id) " +
+              " VALUES (?, ?, ?)                        ";
+  const params = [characterId, id, historyId];
+  return run(sql, params);
 }
---
+++ src/main/model/characters/default-profile.js
@@ -0,0 +1,39 @@
+const defaultProfile = [
+  {
+    title: "Name",
+    entries: ["Namensbedeutung", "Weitere Namen"]
+  },
+  {
+    title: "Physis",
+    entries: ["Alter", "Größe", "Geschlecht", "Gewicht", "Gestalt & Körperbau", "Gesundheit",
+              "Besondere körperliche Merkmale"]
+  },
+  {
+    title: "Aussehen",
+    entries: ["Gesicht", "Augen", "Haare", "Bart", "Kleidung", "Schmuck",
+              "Besondere äußerliche Merkmale"]
+  },
+  {
+    title: "Auftreten",
+    entries: ["Ausstrahlung", "Körperhaltung", "Stimme", "Immer dabei"]
+  },
+  {
+    title: "Gesellschaft",
+    entries: ["Herkunft", "Sprache", "Wohnort", "Beruf", "Stand"]
+  },
+  {
+    title: "Innere Eigenschaften",
+    entries: ["Charakter", "Interessen", "Abneigungen", "Moral", "Träume & Wünsche", "Ziele",
+              "Innerer Konflikt"]
+  },
+  {
+    title: "Veränderungen",
+    entries: ["Kindheit", "Jugend", "Erwachsenenalter", "Hohes Alter"]
+  },
+  {
+    title: "Sonstiges",
+    entries: ["Fähigkeiten", "Anmerkungen"]
+  }
+];
+
+export default defaultProfile;
