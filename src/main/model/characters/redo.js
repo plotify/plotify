4d985febb2570a2a83451776b7e09ddd8d447475
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-04-21 14:16:19 +0200

    #85 (can)RedoCharacterChange: Alle Änderungen innerhalb einer Transaktion. Alle Eingabeparameter werden validiert.

--- src/main/model/characters/redo.js
+++ src/main/model/characters/redo.js
@@ -6,45 +6,13 @@ import {
   addToStack,
   getPresenceContent
 } from "./changes-sequence";
+import { beginTransaction, endTransaction, rollbackTransaction } from "../shared/sqlite";
 
 import { sendCallback } from "../../shared/commons/ipc";
 import {
   CAN_REDO_CHARACTER_CHANGE,
   REDO_CHARACTER_CHANGE
 } from "../../shared/characters/ipc-channels";
-
-export function canRedoCharacterChange(id) {
-  return new Promise((resolve, reject) => {
-
-    if (!id) {
-      throw new Error("No character id was passed.");
-    }
-
-    getStackTop(id, Stack.FUTURE)
-      .then(entry => resolve(entry !== null))
-      .catch(error => reject(error));
-
-  });
-}
-
-export function redoCharacterChange(id) {
-  return new Promise((resolve, reject) => {
-
-    if (!id) {
-      throw new Error("No character id was passed.");
-    }
-
-    let newPresence;
-    getStackTop(id, Stack.FUTURE)
-      .then(entry => { newPresence = entry; return removeFromStack(entry); })
-      .then(() => setNewPresence(newPresence))
-      .then(previousPresence => addToStack(previousPresence, Stack.PAST))
-      .then(() => getPresenceContent(newPresence))
-      .then(content => resolve(content))
-      .catch(error => reject(error));
-
-  });
-}
 
 export function registerRedoCharacterChangeIpcChannels(ipcMain) {
   ipcMain.on(CAN_REDO_CHARACTER_CHANGE, (event, payload) => {
@@ -57,4 +25,35 @@ export function registerRedoCharacterChangeIpcChannels(ipcMain) {
       .then(content => sendCallback(event, payload, content))
      .catch(error => sendCallback(event, payload, error, false));
   });
+}
+
+export function canRedoCharacterChange(characterId) {
+  return Promise.resolve()
+    .then(() => validateParameters(characterId))
+    .then(() => getStackTop(characterId, Stack.FUTURE))
+    .then(entry => Promise.resolve(entry !== null));
+}
+
+export function redoCharacterChange(characterId) {
+  let newPresence;
+  return Promise.resolve()
+    .then(() => validateParameters(characterId))
+    .then(() => beginTransaction())
+    .then(() => getStackTop(characterId, Stack.FUTURE))
+    .then(entry => { newPresence = entry; return removeFromStack(entry); })
+    .then(() => setNewPresence(newPresence))
+    .then(previousPresence => addToStack(previousPresence, Stack.PAST))
+    .then(() => getPresenceContent(newPresence))
+    .then(content => endTransaction(content))
+    .catch(error => {
+      console.log("Failed to redo character change: ", error);
+      return rollbackTransaction(error);
+    });
+}
+
+function validateParameters(characterId) {
+  if (typeof characterId !== "string") {
+    return Promise.reject(new TypeError("No character id was passed as a string: " + characterId));
+  }
+  return Promise.resolve();
 }
