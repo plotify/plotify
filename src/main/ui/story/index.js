a5e5e4d77ff767805989e357a2fb47f79601deda
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-04-22 09:23:49 +0200

    #84 import korrigiert.

--- src/main/ui/story/index.js
+++ src/main/ui/story/index.js
@@ -1,6 +1,6 @@
 import * as actions from "./actions";
 import * as constants from "./constants";
-import * as selectors from "./seletors";
+import * as selectors from "./selectors";
 import reducer from "./reducer";
 
 export default { actions, constants, selectors, reducer };
