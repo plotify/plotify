527bbcb8150ea6645c1e002230f944f7bdb3eb38
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-04-22 16:31:30 +0200

    #84 New selector: getCharactersInOrder

--- src/main/ui/characters/list/selectors.js
+++ src/main/ui/characters/list/selectors.js
@@ -1,3 +1,8 @@
 export function getListFilter(state) {
   return state.characters.list.filter;
 }
+
+export function getCharactersInOrder(state) {
+  return state.characters.list.order.map(
+    id => state.characters.list.characters[id]);
+}
