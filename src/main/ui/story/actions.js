a68344c2e66f9caa5a31c11df585ce11e2789f77
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-04-24 09:25:03 +0200

    #84 Lesbarkeit der Aktionen für das Öffnen und Schließen von Geschichten verbessert.

--- src/main/ui/story/actions.js
+++ src/main/ui/story/actions.js
@@ -5,33 +5,19 @@ import * as c from "../../shared/stories/ipc-channels";
 import { sendToModel } from "../../shared/commons/ipc";
 
 export function openStoryDialog() {
-  return (dispatch, getState) => {
+  return dispatch => {
     return Promise.resolve()
       .then(() => sendToModel(c.OPEN_STORY_DIALOG))
-      .then(file => {
-        if (file) {
-          return dispatch(openStory(file));
-        } else {
-          return Promise.resolve();
-        }
-      })
+      .then(file => dispatch(openStoryIfFileSelected(file)))
       .catch(error => console.log("Fehler beim Öffnen des Dialogs:", error));
   };
 }
 
 export function openStory(file) {
-  return (dispatch, getState) => {
+  return dispatch => {
     return Promise.resolve()
-      .then(() => dispatch(closeStory()))
-      .then(() => {
-        if (!isStoryOpen(getState())) {
-          return Promise.resolve()
-            .then(() => dispatch(openStoryRequest(file)))
-            .then(() => sendToModel(c.OPEN_STORY, file))
-            .then(() => dispatch(openStorySuccessful()))
-            .catch(error => dispatch(openStoryFailed(error)));
-        }
-      });
+      .then(() => dispatch(closeIfStoryIsOpen()))
+      .then(() => dispatch(openStoryIfNoStoryIsOpen(file)));
   };
 }
 
@@ -42,6 +28,34 @@ export function closeStory() {
       .then(() => sendToModel(c.CLOSE_STORY))
       .then(() => dispatch(closeStorySuccessful()))
       .catch(error => dispatch(closeStoryFailed(error)));
+  };
+}
+
+function openStoryIfFileSelected(file) {
+  return dispatch => {
+    if (file) {
+      return dispatch(openStory(file));
+    }
+  };
+}
+
+function closeIfStoryIsOpen() {
+  return (dispatch, getState) => {
+    if (isStoryOpen(getState())) {
+      return dispatch(closeStory());
+    }
+  };
+}
+
+function openStoryIfNoStoryIsOpen(file) {
+  return (dispatch, getState) => {
+    if (!isStoryOpen(getState())) {
+      return Promise.resolve()
+        .then(() => dispatch(openStoryRequest(file)))
+        .then(() => sendToModel(c.OPEN_STORY, file))
+        .then(() => dispatch(openStorySuccessful()))
+        .catch(error => dispatch(openStoryFailed(error)));
+    }
   };
 }
 
