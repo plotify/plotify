33673bb51e64e00b6181d127ca600674c9705e76
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-10-31 20:49:39 +0100

    Button für die Suchfunktion hinzugefügt.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
+import SearchIcon from 'material-ui-icons/Search';
 import CharactersGrid from './characters/CharactersGrid';
 import uuid from 'uuid/v4';
 
@@ -31,7 +32,9 @@ function App(props) {
           <Typography type='title' color='inherit' className={classes.flex}>
             Plotify
           </Typography>
-          <Button color='contrast'>Hello world</Button>
+          <IconButton color='contrast'>
+            <SearchIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
       <div className={classes.content}>
