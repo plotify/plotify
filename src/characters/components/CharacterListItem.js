0917d257afabb6c71860310eec71df147d410847
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-11-03 20:04:31 +0100

    Hevorheben des ausgewählten Charakters in der Liste überarbeitet.

--- src/characters/components/CharacterListItem.js
+++ src/characters/components/CharacterListItem.js
@@ -31,14 +31,24 @@ CharacterListItem.propTypes = {
 
 const styles = theme => {
   const light = theme.palette.type === 'light'
+
   const top = light ? 600 : 700
   const bottom = light ? 100 : 500
+
+  const backgroundColor = light ? theme.palette.secondary['50'] : theme.palette.background.contentFrame
+  const borderColor = light ? theme.palette.secondary['700'] : 'transparent'
+
   return {
     listItem: {
-      height: theme.spacing.unit * 7
+      height: theme.spacing.unit * 7,
+      borderLeftWidth: theme.spacing.unit / 2,
+      borderLeftStyle: 'solid',
+      borderLeftColor: 'transparent',
+      paddingLeft: theme.spacing.unit + theme.spacing.unit / 2
     },
     selectedListItem: {
-      backgroundColor: theme.palette.background.contentFrame,
+      backgroundColor: backgroundColor,
+      borderLeftColor: borderColor,
       boxShadow: 'inset 0 1px 1px -1px ' + theme.palette.grey[top] + ',' +
                 'inset 0 -1px 1px -1px ' + theme.palette.grey[bottom]
     },
