126c4cd1c48bbcc976356c6630ed869763b50d1d
itsJASPERr <itsjasperr@gmail.com>
2017-11-04 00:04:10 +0100

    Ergänzt fehlendes default-profile

---
+++ src/characters/default-profile.js
@@ -0,0 +1,41 @@
+Object.defineProperty(exports, '__esModule', {
+  value: true
+})
+var defaultProfile = [{
+  title: 'Name',
+  entries: [
+    {title: 'Namensbedeutung', value: ''},
+    {title: 'Weitere Namen', value: ''}
+  ]
+}, {
+  title: 'Physis',
+  entries: [
+    {title: 'Alter', value: '18'},
+    {title: 'Größe', value: '182'},
+    {title: 'Geschlecht', value: ''},
+    {title: 'Gewicht', value: ''},
+    {title: 'Gestalt & Körperbau', value: ''},
+    {title: 'Gesundheit', value: ''},
+    {title: 'Besondere körperliche Merkmale', value: 'Er hat keine Beine'}
+  ]
+}, {
+  title: 'Aussehen',
+  entries: ['Gesicht', 'Augen', 'Haare', 'Bart', 'Kleidung', 'Schmuck', 'Besondere äußerliche Merkmale']
+}, {
+  title: 'Auftreten',
+  entries: ['Ausstrahlung', 'Körperhaltung', 'Stimme', 'Immer dabei']
+}, {
+  title: 'Gesellschaft',
+  entries: ['Herkunft', 'Sprache', 'Wohnort', 'Beruf', 'Stand']
+}, {
+  title: 'Innere Eigenschaften',
+  entries: ['Charakter', 'Interessen', 'Abneigungen', 'Moral', 'Träume & Wünsche', 'Ziele', 'Innerer Konflikt']
+}, {
+  title: 'Veränderungen',
+  entries: ['Kindheit', 'Jugend', 'Erwachsenenalter', 'Hohes Alter']
+}, {
+  title: 'Sonstiges',
+  entries: ['Fähigkeiten', 'Anmerkungen']
+}]
+
+exports.default = defaultProfile
