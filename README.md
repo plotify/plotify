2c58fda4fe62d1d3fb6762972b06d73edb7fbbaa
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-11-04 11:21:58 +0100

    Demo optional in Electron anzeigen.

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
 [Demo](http://plotify.org/plotify/)
+
+```bash
+npm install
+npm start
+
+# Optional: Demo in Electron anzeigen.
+# npm start muss parallel ausgeführt werden.
+npm run electron
+```
---
+++ src/electron-starter.js
@@ -0,0 +1,30 @@
+// TODO import syntax
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow.loadURL('http://localhost:3000')
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+});
