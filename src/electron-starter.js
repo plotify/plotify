f10dee5c41c3d58cd557b408764d6b28bd53495b
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-11-06 21:51:22 +0100

    Fensterbreite der Electron-Demo erhöht.

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -16,7 +16,7 @@ console.log(versions)
 let mainWindow
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({ width: 900, height: 600 })
 
   mainWindow.on('closed', () => {
     mainWindow = null
