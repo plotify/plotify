f531c079dfc7e66739d11df86d4d368252ce1ef1
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-11-12 10:36:33 +0100

    Installierte Abhängigkeiten überprüfen.

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,44 @@ const babel = require('gulp-babel')
 const cache = require('gulp-cached')
 const spawn = require('child_process').spawn
 const sequence = require('run-sequence')
+const checkDependencies = require('check-dependencies')
 
 const path = {
   src: './src',
   frontend: './src/frontend',
-  build: './build'
+  build: './build',
+  packageJson: './package.json'
 }
+
+//
+// Preparation
+//
+
+gulp.task('preparation', ['clean', 'check-dependencies'])
 
 gulp.task('clean', () => {
   return gulp
     .src(path.build, {read: false})
     .pipe(clean())
 })
+
+gulp.task('check-dependencies', () => {
+  return checkDependencies().then(result => {
+    if (!result.depsWereOk) {
+      console.log('The following dependencies are not installed in the exact same versions that are specified in package.json:')
+      result.error.forEach(message => {
+        if (!message.includes('to install missing packages')) {
+          console.log(' - ' + message)
+        }
+      })
+      throw new Error('Invoke npm install to install missing packages.')
+    }
+  })
+})
+
+//
+// Compile
+//
 
 gulp.task('compile', () => {
   return gulp
@@ -30,18 +56,30 @@ gulp.task('compile', () => {
     .pipe(gulp.dest(path.build))
 })
 
-gulp.task('assets', () => {
+//
+// Assets
+//
+
+gulp.task('assets', ['static-files', 'package-json'])
+
+gulp.task('static-files', () => {
   return gulp
     .src(path.src + '/**/*.{html,css,sql,png,jpg,jpeg,ico,svg,eot,ttf,woff,woff2,otf}')
-    .pipe(cache('assets', { optimizeMemory: true }))
+    .pipe(cache('static-files', { optimizeMemory: true }))
     .pipe(gulp.dest(path.build))
 })
 
 gulp.task('package-json', () => {
   return gulp
-    .src('./package.json')
+    .src(path.packageJson)
     .pipe(gulp.dest(path.build))
 })
+
+//
+// Development
+//
+
+gulp.task('development', ['electron', 'watch-frontend'])
 
 gulp.task('electron', (callback) => {
   const process = spawn('electron', [path.build + '/electron/main.js'])
@@ -53,6 +91,10 @@ gulp.task('watch-frontend', () => {
   gulp.watch(path.frontend + '/**/*.*', ['compile', 'assets'])
 })
 
+//
+// Combined tasks
+//
+
 gulp.task('default', () => {
-  sequence('clean', 'compile', 'assets', 'package-json', 'watch-frontend', 'electron')
+  sequence('preparation', 'compile', 'assets', 'development')
 })
