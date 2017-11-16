d0b571683c62706cb0952b14bc3d7ea49296ec99
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-11-17 00:04:20 +0100

    Icon für story-Dateien.

--- distribution/linux/after-install.tpl.sh
+++ distribution/linux/after-install.tpl.sh
@@ -4,13 +4,10 @@
 ln -sf '/opt/${productFilename}/${executable}' '/usr/local/bin/${executable}'
 
 
-# Constants
+# Create MIME-Type
 
 mimeType="application/org.plotify.story"
 fileExtension="story"
-
-
-# Create MIME-Type
 
 mimePackagesDirectory="/usr/share/mime/packages"
 mimePackageFile="$mimePackagesDirectory/plotify.xml"
@@ -25,6 +22,7 @@ if [ -d $mimePackagesDirectory ]; then
     <comment xml:lang="en">Plotify story</comment>
     <comment xml:lang="de">Plotify Geschichte</comment>
     <glob pattern="*.$fileExtension"/>
+    <icon name="plotify"/>
   </mime-type>
 </mime-info>
 EndOfFile
@@ -33,38 +31,4 @@ EndOfFile
     sudo update-mime-database /usr/share/mime
   fi
 
-fi
-
-
-# Create file association
-
-mimeTypesFile="/etc/mime.types"
-mimeTypeEntry="$mimeType                   $fileExtension"
-
-if [ -f $mimeTypesFile ]; then
-  if ! grep -q $mimeType $mimeTypesFile; then
-    IFS='%'
-    echo -e $mimeTypeEntry >> $mimeTypesFile
-    unset IFS
-  fi
-fi
-
-
-# Set default application
-
-desktopFile="plotify.desktop"
-defaultsFile="/usr/share/applications/defaults.list"
-defaultEntry="$mimeType=$desktopFile"
-
-if [ -f $defaultsFile ]; then
-  if ! grep -q $mimeType $defaultsFile; then
-    echo -e $defaultEntry >> $defaultsFile
-  fi
-fi
-
-
-# Update desktop database
-
-if type "update-desktop-database" &> /dev/null; then
-  sudo update-desktop-database
 fi
