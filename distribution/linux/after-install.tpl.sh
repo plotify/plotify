e1fa3b629c31c26222fc7a0da62b51a192a840fd
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-11-16 23:41:33 +0100

    Plotify als Standardprogramm für story-Dateien.

--- distribution/linux/after-install.tpl.sh
+++ distribution/linux/after-install.tpl.sh
@@ -4,11 +4,42 @@
 ln -sf '/opt/${productFilename}/${executable}' '/usr/local/bin/${executable}'
 
 
+# Constants
+
+mimeType="application/org.plotify.story"
+fileExtension="story"
+
+
+# Create MIME-Type
+
+mimePackagesDirectory="/usr/share/mime/packages"
+mimePackageFile="$mimePackagesDirectory/plotify.xml"
+
+if [ -d $mimePackagesDirectory ]; then
+
+  cat > $mimePackageFile <<- EndOfFile
+<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+  <mime-type type="$mimeType">
+    <comment>Plotify story</comment>
+    <comment xml:lang="en">Plotify story</comment>
+    <comment xml:lang="de">Plotify Geschichte</comment>
+    <glob pattern="*.$fileExtension"/>
+  </mime-type>
+</mime-info>
+EndOfFile
+
+  if type "update-mime-database" &> /dev/null; then
+    sudo update-mime-database /usr/share/mime
+  fi
+
+fi
+
+
 # Create file association
 
 mimeTypesFile="/etc/mime.types"
-mimeType="application/org.plotify.story"
-mimeTypeEntry="\napplication/org.plotify.story                   story"
+mimeTypeEntry="$mimeType                   $fileExtension"
 
 if [ -f $mimeTypesFile ]; then
   if ! grep -q $mimeType $mimeTypesFile; then
@@ -16,4 +47,24 @@ if [ -f $mimeTypesFile ]; then
     echo -e $mimeTypeEntry >> $mimeTypesFile
     unset IFS
   fi
+fi
+
+
+# Set default application
+
+desktopFile="plotify.desktop"
+defaultsFile="/usr/share/applications/defaults.list"
+defaultEntry="$mimeType=$desktopFile"
+
+if [ -f $defaultsFile ]; then
+  if ! grep -q $mimeType $defaultsFile; then
+    echo -e $defaultEntry >> $defaultsFile
+  fi
+fi
+
+
+# Update desktop database
+
+if type "update-desktop-database" &> /dev/null; then
+  sudo update-desktop-database
 fi
