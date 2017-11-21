dc8f874dd2a709a0d6762122490c3edabcd80397
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-11-21 18:02:45 +0100

    Spec: Database#close()

--- src/backend/shared/sqlite/database.spec.js
+++ src/backend/shared/sqlite/database.spec.js
@@ -1,6 +1,13 @@
 import Database from './database'
 
-// const exampleStatement = 'SELECT foo FROM bar;'
+let connection
+let database
+beforeEach(() => {
+  connection = {
+    close: jest.fn(callback => callback())
+  }
+  database = new Database(connection)
+})
 
 describe('#constructor', () => {
   test('constructs object when called with object', () => {
@@ -17,5 +24,26 @@ describe('#constructor', () => {
 
   test('throws TypeError when called with null', () => {
     expect(() => new Database(null)).toThrow(TypeError)
+  })
+})
+
+describe('#close', () => {
+  test('returns a promise', () => {
+    expect(database.close()).toBeInstanceOf(Promise)
+  })
+
+  test('tries to close the connection', async () => {
+    await database.close()
+    expect(connection.close.mock.calls.length).toBe(1)
+  })
+
+  test('resolves when the connection was closed successfully', () => {
+    return database.close()
+  })
+
+  test('rejects when the connection could not be closed', () => {
+    const error = new Error()
+    connection.close = jest.fn(callback => callback(error))
+    return expect(database.close()).rejects.toBe(error)
   })
 })
