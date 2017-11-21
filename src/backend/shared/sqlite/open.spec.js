264d7494fde8e46f1f781ec4746ece877d4c7062
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-11-21 22:50:27 +0100

    Add test case: resolves to database connection.

--- src/backend/shared/sqlite/open.spec.js
+++ src/backend/shared/sqlite/open.spec.js
@@ -1,8 +1,11 @@
 import { existsSync, unlinkSync } from 'fs'
 
+import Database from './database'
 import mode from './mode'
 import open from './open'
 import { tmpNameSync } from 'tmp'
+
+const createMode = mode.OPEN_CREATE | mode.OPEN_READWRITE
 
 let path
 beforeEach(() => {
@@ -14,9 +17,13 @@ afterEach(() => {
   }
 })
 
-test('creates new file when called with valid path and OPEN_CREATE|OPEN_READWRITE mode', async () => {
+test('resolves to database connection when called with valid path and mode', () => {
+  return expect(open(path, createMode)).resolves.toBeInstanceOf(Database)
+})
+
+test('creates new file when called with valid path and mode OPEN_CREATE | OPEN_READWRITE', async () => {
   expect(existsSync(path)).toBe(false)
-  await open(path, mode.OPEN_CREATE | mode.OPEN_READWRITE)
+  await open(path, createMode)
   expect(existsSync(path)).toBe(true)
 })
 
@@ -26,7 +33,7 @@ test('rejects when file could not be opened', () => {
 })
 
 test('throws TypeError when called with invalid path', () => {
-  expect(() => open(123)).toThrow(TypeError)
+  expect(() => open(123, createMode)).toThrow(TypeError)
 })
 
 test('throws TypeError when called with invalid mode', () => {
