c66f73955b2741f2821531281d8d13a6842c0623
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-11-23 00:00:02 +0100

    Redundant try-catch block removed.

--- src/backend/story/create.js
+++ src/backend/story/create.js
@@ -6,14 +6,10 @@ import path from 'path'
 const statementsFile = path.join(__dirname, 'create.sql')
 
 const create = async (path) => {
-  try {
-    const statements = await fs.readFile(statementsFile, 'utf-8')
-    const database = await open(path, mode.OPEN_CREATE | mode.OPEN_READWRITE)
-    await database.exec(statements)
-    await database.close()
-  } catch (error) {
-    throw error
-  }
+  const statements = await fs.readFile(statementsFile, 'utf-8')
+  const database = await open(path, mode.OPEN_CREATE | mode.OPEN_READWRITE)
+  await database.exec(statements)
+  await database.close()
 }
 
 export default create
