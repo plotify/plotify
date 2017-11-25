0fc4dc3376cb1b810ce4f4bbbf1253fcbed0830b
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-11-25 13:44:01 +0100

    Only open https and http URLs.

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill'
 
 import { BrowserWindow, Menu, app, shell } from 'electron'
+import { URL, format } from 'url'
 import { registerRequestHandlers, setShouldSaveState } from './saved-state'
 
 import { createMenuTemplate } from './menu'
@@ -9,7 +10,6 @@ import isDev from 'electron-is-dev'
 import path from 'path'
 import printWelcomeScreen from './versions'
 import { setMainWindow } from './shared/main-window'
-import url from 'url'
 
 printWelcomeScreen()
 setShouldSaveState(isDev)
@@ -32,10 +32,13 @@ const createWindow = () => {
 
   mainWindow.webContents.on('new-window', (event, url) => {
     event.preventDefault()
-    shell.openExternal(url)
+    const protocol = new URL(url).protocol
+    if (protocol === 'https:' || protocol === 'http:') {
+      shell.openExternal(url)
+    }
   })
 
-  mainWindow.loadURL(url.format({
+  mainWindow.loadURL(format({
     pathname: path.join(__dirname, '../frontend/static/index.html'),
     protocol: 'file:',
     slashes: true
