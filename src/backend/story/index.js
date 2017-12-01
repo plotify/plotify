296f745692b1cb203aa7aedfbe60c8ab951d446c
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-12-01 01:13:41 +0100

    Export Story class.

--- src/backend/story/index.js
+++ src/backend/story/index.js
@@ -1,1 +1,2 @@
+export { default as Story } from './story'
 export { default as createStory } from './create'
--- src/backend/story/index.spec.js
+++ src/backend/story/index.spec.js
@@ -1,4 +1,8 @@
-import { createStory } from './'
+import { Story, createStory } from './'
+
+test('exports Story class', () => {
+  expect(Story).toBeInstanceOf(Function)
+})
 
 test('exports createStory function', () => {
   expect(createStory).toBeInstanceOf(Function)
