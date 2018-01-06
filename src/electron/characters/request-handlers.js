abb7c47d7f32e11854731940ad2ad9c7a90e5772
Sebastian Schmidt <email@sebastian-schmidt.info>
2018-01-06 16:05:40 +0100

    #112 Request handler: UPDATE_ENTRY

--- src/electron/characters/request-handlers.js
+++ src/electron/characters/request-handlers.js
@@ -1,5 +1,5 @@
-import { CREATE_CHARACTER, FIND_CHARACTERS, GET_PROFILE } from '../../shared/characters/requests'
-import { createCharacter, findCharacters, getProfile } from '../../backend/characters'
+import { CREATE_CHARACTER, FIND_CHARACTERS, GET_PROFILE, UPDATE_ENTRY } from '../../shared/characters/requests'
+import { createCharacter, findCharacters, getProfile, updateEntry } from '../../backend/characters'
 
 import { getStoryByWindow } from '../story'
 import { requestHandler } from '../shared/communication'
@@ -7,28 +7,36 @@ import { requestHandler } from '../shared/communication'
 const handleCreateCharacter = (resolve, reject, senderWindow, name) => {
   const database = getStoryByWindow(senderWindow).database
   createCharacter(database, name)
-    .then(characterId => resolve(characterId))
-    .catch(error => reject(error.message))
+    .then((characterId) => resolve(characterId))
+    .catch((error) => reject(error.message))
 }
 
 const handleFindCharacters = (resolve, reject, senderWindow, { deleted, filter }) => {
   const database = getStoryByWindow(senderWindow).database
   findCharacters(database, deleted, filter)
-    .then(characters => resolve(characters))
-    .catch(error => reject(error.message))
+    .then((characters) => resolve(characters))
+    .catch((error) => reject(error.message))
 }
 
 const handleGetProfile = (resolve, reject, senderWindow, characterId) => {
   const database = getStoryByWindow(senderWindow).database
   getProfile(database, characterId)
-    .then(profile => resolve(profile))
-    .catch(error => reject(error.message))
+    .then((profile) => resolve(profile))
+    .catch((error) => reject(error.message))
+}
+
+const handleUpdateEntry = (resolve, reject, senderWindow, entry) => {
+  const database = getStoryByWindow(senderWindow).database
+  updateEntry(database, entry)
+    .then(() => resolve())
+    .catch((error) => reject(error.message))
 }
 
 const registerRequestHandlers = () => {
   requestHandler(CREATE_CHARACTER, handleCreateCharacter)
   requestHandler(FIND_CHARACTERS, handleFindCharacters)
   requestHandler(GET_PROFILE, handleGetProfile)
+  requestHandler(UPDATE_ENTRY, handleUpdateEntry)
 }
 
 export default registerRequestHandlers
--- src/shared/characters/requests.js
+++ src/shared/characters/requests.js
@@ -1,3 +1,4 @@
 export const CREATE_CHARACTER = 'characters/create-character'
 export const FIND_CHARACTERS = 'characters/find-characters'
 export const GET_PROFILE = 'characters/get-profile'
+export const UPDATE_ENTRY = 'characters/update-entry'
