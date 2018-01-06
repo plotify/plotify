055352a0c2a22ceb2e7be87f0b906b0b5da15e55
Sebastian Schmidt <email@sebastian-schmidt.info>
2018-01-06 13:53:05 +0100

    #112 Refactoring: addChange

--- src/backend/shared/entity-changes/add-change.js
+++ src/backend/shared/entity-changes/add-change.js
@@ -1,8 +1,42 @@
-// import { getPresenceHistoryId, setPresenceHistoryId } from './presence-history-id'
+import { Stack, deleteStack, getStack, pushToStack } from './stack'
+import { getPresenceHistoryId, setPresenceHistoryId } from './presence-history-id'
 
-const addChange = async (database, type, characterId, typeId, historyId) => {
-  // const prevHistoryId = await getPresenceHistoryId(database, type, typeId)
-  // await setPresenceHistoryId(database, type, typeId, historyId)
+const addChange = async (transaction, type, entityId, typeId, historyId) => {
+  const prevHistoryId = await getPresenceHistoryId(transaction, type, typeId)
+  await setPresenceHistoryId(transaction, type, typeId, historyId)
+  await modifyChangeSequence(transaction, type, entityId, typeId, prevHistoryId)
+}
+
+const modifyChangeSequence = async (transaction, type, entityId, typeId, prevHistoryId) => {
+  await pushToStack(Stack.PAST, transaction, type, entityId, typeId, prevHistoryId)
+  const futureStack = await getStack(Stack.FUTURE, transaction, type, entityId)
+  if (futureStack.length > 0) {
+    await handleFuture(transaction, type, entityId, typeId, prevHistoryId, futureStack)
+  }
+}
+
+const handleFuture = async (transaction, type, entityId, typeId, prevHistoryId, futureStack) => {
+  await pushTopToBottomFutureToPast(transaction, type, futureStack)
+  await pushSecondFromBottomFutureToPast(transaction, type, futureStack)
+  await pushToStack(Stack.PAST, transaction, type, entityId, typeId, prevHistoryId)
+  await deleteStack(Stack.FUTURE, transaction, type, entityId)
+}
+
+const pushTopToBottomFutureToPast = async (transaction, type, futureStack) => {
+  for (let index = futureStack.length - 1; index >= 0; index--) {
+    await pushEntryToPastStack(transaction, type, futureStack[index])
+  }
+}
+
+const pushSecondFromBottomFutureToPast = async (transaction, type, futureStack) => {
+  for (let index = 1; index < futureStack.length; index++) {
+    await pushEntryToPastStack(transaction, type, futureStack[index])
+  }
+}
+
+const pushEntryToPastStack = async (transaction, type, entry) => {
+  const entryType = { ...type, id: entry.type } // Temporary invalid type.
+  await pushToStack(Stack.PAST, transaction, entryType, entry.entityId, entry.historyId)
 }
 
 export default addChange
--- src/backend/shared/entity-changes/stack.js
+++ src/backend/shared/entity-changes/stack.js
@@ -19,6 +19,16 @@ const topSql = (type) => {
   `
 }
 
+const topPositionSql = (type) => {
+  const sequenceTable = type.sequenceTable.name
+  const entityId = type.sequenceTable.entityIdColumn
+  return `
+    SELECT max(position) AS maxPosition
+    FROM ${sequenceTable}
+    WHERE ${entityId} = ? AND stack = ?
+  `
+}
+
 const removeSql = (type) => {
   const sequenceTable = type.sequenceTable.name
   const entityId = type.sequenceTable.entityIdColumn
@@ -33,11 +43,50 @@ const removeSql = (type) => {
   `
 }
 
-export const getStackTop = async (database, type, entityId, stack) => {
+const stackSql = (type) => {
+  const sequenceTable = type.sequenceTable.name
+  const entityId = type.sequenceTable.entityIdColumn
+  return `
+    SELECT ${entityId} AS entityId, stack, position, type,
+           type_id AS typeId, history_id AS historyId
+    FROM ${sequenceTable}
+    WHERE ${entityId} = ? AND stack = ?
+    ORDER BY position ASC
+  `
+}
+
+const addSql = (type) => {
+  const sequenceTable = type.sequenceTable.name
+  const entityId = type.sequenceTable.entityIdColumn
+  return `
+    INSERT INTO ${sequenceTable}
+    (position, ${entityId}, type, type_id, history_id, stack)
+    VALUES (?, ?, ?, ?, ?, ?)
+  `
+}
+
+const deleteSql = (type) => {
+  const sequenceTable = type.sequenceTable.name
+  const entityId = type.sequenceTable.entityIdColumn
+  return `
+    DELETE FROM ${sequenceTable}
+    WHERE ${entityId} = ? AND stack = ?
+  `
+}
+
+export const getStackTop = async (stack, database, type, entityId) => {
   validateType(type)
   const sql = topSql(type)
   const params = [entityId, stack, entityId, stack]
   return database.get(sql, params)
+}
+
+export const getStackTopPosition = async (stack, database, type, entityId) => {
+  validateType(type)
+  const sql = topPositionSql(type)
+  const params = [entityId, stack]
+  const result = await database.get(sql, params)
+  return result.maxPosition
 }
 
 export const removeFromStack = async (database, type, entry) => {
@@ -51,5 +100,29 @@ export const removeFromStack = async (database, type, entry) => {
     entry.typeId,
     entry.historyId
   ]
+  return database.run(sql, params)
+}
+
+export const getStack = async (stack, database, type, entityId) => {
+  validateType(type)
+  const sql = stackSql(type)
+  const params = [entityId, stack]
+  return database.all(sql, params)
+}
+
+export const pushToStack = async (stack, database, type, entityId, typeId, historyId) => {
+  validateType(type)
+  const topPosition = await getStackTopPosition(stack, database, type, entityId)
+  const nextPosition = topPosition === null ? 0 : topPosition + 1
+  const sql = addSql(type)
+  const params = [nextPosition, entityId, type.id, typeId, historyId, stack]
+  await database.run(sql, params)
+  return nextPosition
+}
+
+export const deleteStack = async (stack, database, type, entityId) => {
+  validateType(type)
+  const sql = deleteSql(type)
+  const params = [entityId, stack]
   return database.run(sql, params)
 }
