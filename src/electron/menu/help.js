2d46e42d708f8a6254e2da2b9fe519b4210481b2
Sebastian Schmidt <email@sebastian-schmidt.info>
2018-01-14 01:00:24 +0100

    #138 Links zum Repository und zum Bugtracker

--- src/electron/menu/help.js
+++ src/electron/menu/help.js
@@ -1,13 +1,16 @@
 import { OPEN_ABOUT_DIALOG } from '../../shared/about/requests'
 import { request } from '../shared/communication'
+import { shell } from 'electron'
 
-// TODO Feedback geben
 const help = () => ({
   label: 'Hilfe',
   submenu: [
     {
       label: 'Entwicklung',
       submenu: [
+        { label: 'GitHub', click: openGitHub },
+        { label: 'Fehler melden', click: reportBug },
+        { type: 'separator' },
         { label: 'Werkzeuge', role: 'toggledevtools' }
       ]
     },
@@ -18,6 +21,16 @@ const help = () => ({
 
 const openAboutDialog = (_, window, __) => {
   request(window, OPEN_ABOUT_DIALOG)
+}
+
+const openGitHub = () => {
+  const packageJson = require('../../package.json')
+  shell.openExternal(packageJson.repository.url)
+}
+
+const reportBug = () => {
+  const packageJson = require('../../package.json')
+  shell.openExternal(packageJson.bugs.url)
 }
 
 export default help
