b6d2496213b27b8ec6e4f500e50981d29e882c5e
Sebastian Schmidt <email@sebastian-schmidt.info>
2018-01-31 00:40:49 +0100

    :fire: Remove empty profile placeholder image (#143)

--- src/frontend/characters/components/ProfileEmptyHint.js
+++ src/frontend/characters/components/ProfileEmptyHint.js
@@ -1,25 +1,8 @@
 import React from 'react'
 import Typography from 'material-ui/Typography'
-import { withStyles } from 'material-ui/styles'
-
-const styles = theme => ({
-  editInline: {
-    position: 'absolute',
-    margin: '-3px 3px'
-  },
-  mascot: {
-    width: 200,
-    margin: 15,
-    opacity: 0.7
-  }
-})
 
 const ProfileEmptyHint = (props) => (
   <div className={props.className}>
-    <img
-      src='./squirrel.png'
-      className={props.classes.mascot}
-    />
     <Typography>
       Es liegen noch keine Einträge im Steckbrief dieses Charakters vor.
     </Typography>
@@ -29,4 +12,4 @@ const ProfileEmptyHint = (props) => (
   </div>
 )
 
-export default withStyles(styles)(ProfileEmptyHint)
+export default ProfileEmptyHint
