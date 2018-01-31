3ea418e14371260ed12e7daa3989adbc830844e8
Sebastian Schmidt <email@sebastian-schmidt.info>
2018-02-01 00:46:23 +0100

    :art: Improve formatting of actions and the reducer (#144)

--- src/electron/splash-screen/actions.js
+++ src/electron/splash-screen/actions.js
@@ -4,41 +4,33 @@ import { getNumberOfLoadingProccesses, getWindow } from './selectors'
 
 import create from './create'
 
-export const showSplashScreen = () => {
-  return (dispatch, getState) => {
-    dispatch(incrementLoadingProcesses())
-    const window = getWindow(getState())
-    if (window === null) {
-      dispatch(setWindow(create()))
-    } else {
-      window.restore()
-      window.focus()
-    }
+export const showSplashScreen = () => (dispatch, getState) => {
+  dispatch(incrementLoadingProcesses())
+  const window = getWindow(getState())
+  if (window === null) {
+    const newWindow = create()
+    dispatch(setWindow(newWindow))
+  } else {
+    window.restore()
+    window.focus()
   }
 }
 
-export const focusSplashScreenIfExisting = () => {
-  return (dispatch, getState) => {
-    const state = getState()
-    const window = getWindow(state)
-    const loadingProccesses = getNumberOfLoadingProccesses(state)
-    if (window !== null && loadingProccesses > 0) {
-      window.restore()
-      window.focus()
-    }
+export const focusSplashScreenIfExisting = () => (dispatch, getState) => {
+  const window = getWindow(getState())
+  const loadingProccesses = getNumberOfLoadingProccesses(getState())
+  if (window !== null && loadingProccesses > 0) {
+    window.restore()
+    window.focus()
   }
 }
 
-export const closeSplashScreen = () => {
-  return (dispatch, getState) => {
-    dispatch(decrementLoadingProcesses())
-    const state = getState()
-    const window = getWindow(state)
-    const loadingProccesses = getNumberOfLoadingProccesses(state)
-    if (loadingProccesses === 0) {
-      window.close()
-      dispatch(setWindow(null))
-    }
+export const closeSplashScreen = () => (dispatch, getState) => {
+  dispatch(decrementLoadingProcesses())
+  const loadingProccesses = getNumberOfLoadingProccesses(getState())
+  if (loadingProccesses === 0) {
+    getWindow(getState()).close()
+    dispatch(setWindow(null))
   }
 }
 
--- src/electron/splash-screen/reducer.js
+++ src/electron/splash-screen/reducer.js
@@ -8,15 +8,21 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case t.INCREMENT_LOADING_PROCESSES:
-      return { ...state, loadingProcesses: state.loadingProcesses + 1 }
+      const loadingProcesses = state.loadingProcesses + 1
+      return { ...state, loadingProcesses }
+
     case t.DECREMENT_LOADING_PROCESSES:
       if (state.loadingProcesses > 0) {
-        return { ...state, loadingProcesses: state.loadingProcesses - 1 }
+        const loadingProcesses = state.loadingProcesses - 1
+        return { ...state, loadingProcesses }
       } else {
         return state
       }
+
     case t.SET_WINDOW:
-      return { ...state, window: action.payload.window }
+      const window = action.payload.window
+      return { ...state, window }
+
     default:
       return state
   }
