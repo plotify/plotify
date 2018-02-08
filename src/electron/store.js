18a9c7656926c31552f2fe77fd8da1a2a01f8501
Sebastian Schmidt <email@sebastian-schmidt.info>
2018-02-08 20:53:46 +0100

    :loud_sound: Log state while in development mode (#144)

--- src/electron/store.js
+++ src/electron/store.js
@@ -1,6 +1,18 @@
 import { applyMiddleware, createStore } from 'redux'
 
+import isDev from 'electron-is-dev'
 import reducers from './reducers'
 import thunk from 'redux-thunk'
 
-export default createStore(reducers, applyMiddleware(thunk))
+const store = createStore(reducers, applyMiddleware(thunk))
+
+if (isDev) {
+  store.subscribe(() => {
+    const separator = '---------------------------------------------------------------------'
+    console.log(separator + '\n')
+    console.log(store.getState())
+    console.log('\n' + separator)
+  })
+}
+
+export default store
