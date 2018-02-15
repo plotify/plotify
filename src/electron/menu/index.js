4fda4f7f635c8e2b4b0f2568463b337db5d889e3
Sebastian Schmidt <email@sebastian-schmidt.info>
2018-02-15 18:26:15 +0100

    :sparkles: Show windows in menu bar (#154)

--- src/electron/menu/index.js
+++ src/electron/menu/index.js
@@ -8,9 +8,10 @@ import file from './file'
 import help from './help'
 import store from '../store'
 import view from './view'
+import windows from './windows'
 
 const templateCreator = createSelector(
-  file, edit, view, help,
+  file, edit, view, windows, help,
   (...categories) => [...categories]
 )
 
