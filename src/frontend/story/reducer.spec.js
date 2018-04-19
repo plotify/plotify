e2a34e66e2189dc0aa78bd5d7faa608cf2bb62f0
Sebastian Schmidt <email@sebastian-schmidt.info>
2018-04-19 09:51:44 +0200

    :recycle: Uniform story status dialogs (#194)

--- src/frontend/story/reducer.spec.js
+++ src/frontend/story/reducer.spec.js
@@ -1,5 +1,5 @@
-import { closeCreateStoryDialog, closeOpenStoryDialog, closeStoryPreparationStarted, createStoryFailed, createStoryStarted, createStorySuccessful, openStoryFailed, openStoryStarted, openStorySuccessful, _storyClosed as storyClosed } from './actions'
-import { getStoryPath, isClosingStory, isCreatingStory, isOpeningStory, isShowCreateStoryDialog, isShowOpenStoryDialog, isStoryOpen } from './selectors'
+import { closeStoryPreparationStarted, createStoryFailed, createStoryStarted, createStorySuccessful, openStoryFailed, openStoryStarted, openStorySuccessful, _storyClosed as storyClosed } from './actions'
+import { getStoryPath, isClosingStory, isCreatingStory, isOpeningStory } from './selectors'
 
 import { createStore } from '../store'
 
@@ -14,60 +14,44 @@ test('initial state', () => {
 
 const validateInitialState = () => {
   const state = store.getState()
-  expect(isStoryOpen(state)).toBe(false)
   expect(getStoryPath(state)).toBe(null)
   expect(isOpeningStory(state)).toBe(false)
-  expect(isShowOpenStoryDialog(state)).toBe(false)
   expect(isCreatingStory(state)).toBe(false)
-  expect(isShowCreateStoryDialog(state)).toBe(false)
   expect(isClosingStory(state)).toBe(false)
 }
 
 test('openStoryStarted', async () => {
-  await store.dispatch(openStoryStarted())
+  const path = '/hello/world.story'
+  await store.dispatch(openStoryStarted(path))
 
   const state = store.getState()
+  expect(getStoryPath(state)).toBe(path)
   expect(isOpeningStory(state)).toBe(true)
-  expect(isShowOpenStoryDialog(state)).toBe(true)
-  expect(isShowCreateStoryDialog(state)).toBe(false)
 })
 
 test('openStorySuccessful', async () => {
-  await store.dispatch(openStoryStarted())
-
   const path = '/hello/world.story'
-  await store.dispatch(openStorySuccessful(path))
+  await store.dispatch(openStoryStarted(path))
+
+  await store.dispatch(openStorySuccessful())
 
   const state = store.getState()
-  expect(isStoryOpen(state)).toBe(true)
   expect(getStoryPath(state)).toBe(path)
   expect(isOpeningStory(state)).toBe(false)
 })
 
 test('openStoryFailed', async () => {
-  await store.dispatch(openStoryStarted())
+  await store.dispatch(openStoryStarted('/hello/world.story'))
   await store.dispatch(openStoryFailed())
 
   const state = store.getState()
-  expect(isStoryOpen(state)).toBe(false)
   expect(getStoryPath(state)).toBe(null)
   expect(isOpeningStory(state)).toBe(false)
 })
 
-test('closeOpenStoryDialog', async () => {
-  await store.dispatch(openStoryStarted())
-  expect(isShowOpenStoryDialog(store.getState())).toBe(true)
-  await store.dispatch(closeOpenStoryDialog())
-  expect(isShowOpenStoryDialog(store.getState())).toBe(false)
-})
-
 test('createStoryStarted', async () => {
   await store.dispatch(createStoryStarted())
-
-  const state = store.getState()
-  expect(isCreatingStory(state)).toBe(true)
-  expect(isShowCreateStoryDialog(state)).toBe(true)
-  expect(isShowOpenStoryDialog(state)).toBe(false)
+  expect(isCreatingStory(store.getState())).toBe(true)
 })
 
 test('createStorySuccessful', async () => {
@@ -79,34 +63,19 @@ test('createStorySuccessful', async () => {
 test('createStoryFailed', async () => {
   await store.dispatch(createStoryStarted())
   await store.dispatch(createStoryFailed())
-
-  const state = store.getState()
-  expect(isCreatingStory(state)).toBe(false)
-  expect(isShowCreateStoryDialog(state)).toBe(true)
-})
-
-test('closeCreateStoryDialog', async () => {
-  await store.dispatch(createStoryStarted())
-  expect(isShowCreateStoryDialog(store.getState())).toBe(true)
-  await store.dispatch(closeCreateStoryDialog())
-  expect(isShowCreateStoryDialog(store.getState())).toBe(false)
+  expect(isCreatingStory(store.getState())).toBe(false)
 })
 
 test('closeStoryPreparationStarted', async () => {
-  await store.dispatch(openStoryStarted())
-  await store.dispatch(openStorySuccessful('/hello/world.story'))
+  await store.dispatch(openStoryStarted('/hello/world.story'))
+  await store.dispatch(openStorySuccessful())
   await store.dispatch(closeStoryPreparationStarted())
-
-  const state = store.getState()
-  expect(isStoryOpen(state)).toBe(true)
-  expect(isClosingStory(state)).toBe(true)
-  expect(isShowOpenStoryDialog(state)).toBe(false)
-  expect(isShowCreateStoryDialog(state)).toBe(false)
+  expect(isClosingStory(store.getState())).toBe(true)
 })
 
 test('storyClosed', async () => {
-  await store.dispatch(openStoryStarted())
-  await store.dispatch(openStorySuccessful('/hello/world.story'))
+  await store.dispatch(openStoryStarted('/hello/world.story'))
+  await store.dispatch(openStorySuccessful())
   await store.dispatch(closeStoryPreparationStarted())
 
   await store.dispatch(storyClosed())
--- src/frontend/story/selectors.js
+++ src/frontend/story/selectors.js
@@ -1,20 +1,11 @@
 export const isOpeningStory = (state) =>
   state.story.openingStory === true
 
-export const isShowOpenStoryDialog = (state) =>
-  state.story.showOpenStoryDialog === true
-
-export const isStoryOpen = (state) =>
-  state.story.openStory !== null
-
 export const getStoryPath = (state) =>
-  state.story.openStory
+  state.story.storyPath
 
 export const isCreatingStory = (state) =>
   state.story.creatingStory === true
-
-export const isShowCreateStoryDialog = (state) =>
-  state.story.showCreateStoryDialog === true
 
 export const isClosingStory = (state) =>
   state.story.closingStory === true
